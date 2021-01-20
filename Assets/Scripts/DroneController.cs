6954aa9988b70d27d8dc2159b80a1edb0258443f
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2021-01-20 13:08:30 +0530

    Drone Setup to Fly

---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour
+{
+    Animator _Anim;
+
+    Vector3 _Speed = new Vector3(0.0f, 0.0f, 0.0f);
+
+    public float _SpeedMultiplyer = 1.0f;
+
+    void Start()
+    {
+        _Anim = GetComponent<Animator>();
+        _Anim.SetBool("TakeOff", true); 
+    }
+
+    public void Move(float _speedX, float _speedZ)
+    {
+        _Speed.x = _speedX;
+        _Speed.z = _speedZ;
+
+        UpdateDrone();
+    }
+
+    void UpdateDrone()
+    {
+        transform.localPosition += _Speed * _SpeedMultiplyer * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public DroneController _DroneController;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float speedX = Input.GetAxis("Horizontal");
+        float speedZ = Input.GetAxis("Vertical");
+
+        _DroneController.Move(speedX, speedZ);
+    }
+}
