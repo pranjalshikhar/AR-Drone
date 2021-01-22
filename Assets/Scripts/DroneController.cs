c03496028e544fe9c3a129bf62c302ba6b1fad72
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2021-01-22 16:39:06 +0530

    OnClick Button Activated

--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -11,6 +11,10 @@ public class DroneController : MonoBehaviour
         DRONE_STATE_TAKINGOFF,
         DRONE_STATE_MOVING_UP,
         DRONE_STATE_FLYING,
+        DRONE_STATE_START_LANDING,
+        DRONE_STATE_LANDING,
+        DRONE_STATE_LANDED,
+        DRONE_STATE_WAIT_ENGINE_STOP
     }
 
     DroneState _State;
@@ -29,6 +33,16 @@ public class DroneController : MonoBehaviour
     public void TakeOff()
     {
         _State = DroneState.DRONE_STATE_START_TAKINGOFF;
+    }
+
+    public bool IsFlying()
+    {
+        return (_State == DroneState.DRONE_STATE_FLYING);
+    }
+
+    public void Land()
+    {
+        _State = DroneState.DRONE_STATE_START_LANDING;
     }
 
     void Start()
@@ -65,7 +79,7 @@ public class DroneController : MonoBehaviour
                 break;
 
             case DroneState.DRONE_STATE_MOVING_UP:
-                if(_Anim.GetBool("TakeOff") == false)
+                if(_Anim.GetBool("MoveUp") == false)
                 {
                     _State = DroneState.DRONE_STATE_FLYING;
                 }
@@ -78,7 +92,30 @@ public class DroneController : MonoBehaviour
                 Vector3 rotation = transform.localRotation.eulerAngles;
                 transform.localPosition += _Speed * _SpeedMultiplyer * Time.deltaTime;
                 transform.localRotation = Quaternion.Euler(angleX, rotation.y, angleZ);
+                break;
 
+            case DroneState.DRONE_STATE_START_LANDING:
+                _Anim.SetBool("MoveDown", true);
+                _State = DroneState.DRONE_STATE_LANDING;
+                break;
+
+            case DroneState.DRONE_STATE_LANDING:
+                if(_Anim.GetBool("MoveDown") == false)
+                {
+                    _State = DroneState.DRONE_STATE_LANDED;
+                }
+                break;
+
+            case DroneState.DRONE_STATE_LANDED:
+                _Anim.SetBool("Land", true);
+                _State = DroneState.DRONE_STATE_WAIT_ENGINE_STOP;
+                break;
+
+            case DroneState.DRONE_STATE_WAIT_ENGINE_STOP:
+                if(_Anim.GetBool("Land") == false)
+                {
+                    _State = DroneState.DRONE_STATE_IDLE;
+                }
                 break;
         }
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,19 +6,72 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public DroneController _DroneController;
+    
     public Button _FlyButton;
+    public Button _LandButton;
+
+    public GameObject _Controls;
+
+    struct DroneAnimationControls
+    {
+        public bool _moving;
+        public bool _interpolatingAsc;
+        public bool _interpolatingDesc;
+        public float _axis;
+        public float _direction;
+    }
+
+    DroneAnimationControls _MovingLeft;
+    // DroneAnimationControls _MovingRight;
+    DroneAnimationControls _MovingBack;
+    // DroneAnimationControls _MovingForward;
 
     void Start()
     {
         _FlyButton.onClick.AddListener(EventClickFlyButton);
+        _LandButton.onClick.AddListener(EventOnClickLandButton);
+    }
+
+    void UpdateControls(ref DroneAnimationControls _controls)
+    {
+        if(_controls._moving || _controls._interpolatingAsc || _controls._interpolatingDesc)
+        {
+            if(_controls._interpolatingAsc)
+            {
+                _controls._axis += 0.05f;
+
+                if(_controls._axis >= 1.0f)
+                {
+                    _controls._axis = 1.0f;
+                    _controls._interpolatingAsc = false;
+                    _controls._interpolatingDesc = true;
+                }
+            }
+            else if(!_controls._moving)
+            {
+                _controls._axis -= 0.05f;
+
+                if (_controls._axis <= 0.0f)
+                {
+                    _controls._axis = 0.0f;
+                    _controls._interpolatingDesc = false;
+                    /*_controls._interpolatingAsc = true;*/
+                }
+            }
+        }
     }
 
     void Update()
     {
-        float speedX = Input.GetAxis("Horizontal");
-        float speedZ = Input.GetAxis("Vertical");
+        // float speedX = Input.GetAxis("Horizontal");
+        // float speedZ = Input.GetAxis("Vertical");
 
-        _DroneController.Move(speedX, speedZ);
+        UpdateControls(ref _MovingLeft);
+        // UpdateControls(ref _MovingRight);
+        UpdateControls(ref _MovingBack);
+        // UpdateControls(ref _MovingForward);
+
+        _DroneController.Move(_MovingLeft._axis * _MovingLeft._direction, _MovingBack._axis * _MovingBack._direction);
     }
 
     void EventClickFlyButton()
@@ -27,6 +80,74 @@ public class GameManager : MonoBehaviour
         {
             _DroneController.TakeOff();
             _FlyButton.gameObject.SetActive(false);
+            _Controls.SetActive(true); 
         }
     }
+
+    void EventOnClickLandButton()
+    {
+        if(_DroneController.IsFlying())
+        {
+            _DroneController.Land();
+            _LandButton.gameObject.SetActive(true);
+            _Controls.SetActive(false);
+            _FlyButton.gameObject.SetActive(true);
+        }
+    }
+    
+    // Left Button
+    public void EventOnLeftButtonPressed()
+    {
+        _MovingLeft._moving = true;
+        _MovingLeft._interpolatingAsc = true;
+        _MovingLeft._direction = -1.0f;
+    }
+
+    public void EventOnLeftButtonReleased()
+    {
+        _MovingLeft._moving = false;
+    }
+
+
+    // Right Button
+    public void EventOnRightButtonPressed()
+    {
+        _MovingLeft._moving = true;
+        _MovingLeft._interpolatingAsc = true;
+        _MovingLeft._direction = 1.0f;
+    }
+
+    public void EventOnRightButtonReleased()
+    {
+        _MovingLeft._moving = false;
+    }
+
+
+    // Back Button
+    public void EventOnBackButtonPressed()
+    {
+        _MovingBack._moving = true;
+        _MovingBack._interpolatingAsc = true;
+        _MovingBack._direction = -1.0f;
+    }
+
+    public void EventOnBackButtonReleased()
+    {
+        _MovingBack._moving = false;
+    }
+
+
+    // Forward Button
+    public void EventOnForwardButtonPressed()
+    {
+        _MovingBack._moving = true;
+        _MovingBack._interpolatingAsc = true;
+        _MovingBack._direction = 1.0f;
+    }
+
+    public void EventOnForwardButtonReleased()
+    {
+        _MovingBack._moving = false;
+    }
+
 }
